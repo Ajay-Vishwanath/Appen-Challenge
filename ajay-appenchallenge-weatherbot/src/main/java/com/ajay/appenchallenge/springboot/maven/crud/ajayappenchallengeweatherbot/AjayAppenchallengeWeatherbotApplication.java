171d64073d618a790c5731f0a70b08f2a4f561a9
Ajay <ajayvishwanath.94@gmail.com>
2020-10-20 16:58:05 -0700

    Create Backend and Frontend Skeletons

---
+++ ajay-appenchallenge-weatherbot/src/main/java/com/ajay/appenchallenge/springboot/maven/crud/ajayappenchallengeweatherbot/AjayAppenchallengeWeatherbotApplication.java
@@ -0,0 +1,13 @@
+package com.ajay.appenchallenge.springboot.maven.crud.ajayappenchallengeweatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjayAppenchallengeWeatherbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjayAppenchallengeWeatherbotApplication.class, args);
+	}
+
+}
---
+++ ajay-appenchallenge-weatherbot/src/test/java/com/ajay/appenchallenge/springboot/maven/crud/ajayappenchallengeweatherbot/AjayAppenchallengeWeatherbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.appenchallenge.springboot.maven.crud.ajayappenchallengeweatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjayAppenchallengeWeatherbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
